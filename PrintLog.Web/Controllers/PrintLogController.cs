e06b3fbceb0c482f805047a5c98611180315a1f1
Piyakorn Ungamrung <piyakorn.u@gofive.co.th>
2020-05-19 18:38:26 +0700

    add web project

---
+++ PrintLog.Web/Controllers/PrintLogController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrintLog.Web.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class PrintLogController : ControllerBase {
+
+        private readonly ILogger<PrintLogController> _logger;
+
+        public PrintLogController(ILogger<PrintLogController> logger) {
+            _logger = logger;
+        }
+
+        
+    }
+}
